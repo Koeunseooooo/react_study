60e5731b7d99cf0bac37814edc2032544a7eb3c5
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-03-31 18:01:13 +0900

    node.js 패키지 생성 및 외부 모듈 이용하기

---
+++ index.js
@@ -0,0 +1,11 @@
+const randomColor = require('randomcolor'); // import the script
+// 외부경로를 통해 다운로드 받은 모듈은, require 인자로 모듈이름만 적으면 됨
+
+let color1 = randomColor();
+
+let color2 = randomColor();
+
+let color3 = randomColor();
+
+
+console.log(color1,color2,color3);
